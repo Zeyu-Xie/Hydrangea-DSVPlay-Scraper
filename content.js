bdd9802801b9ded314ac80aba33bc662d8317e76
Zeyu Xie <xie.zeyu20@gmail.com>
2025-03-31 09:45:58 +0200

    .

--- content.js
+++ content.js
@@ -1,17 +1,29 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.action === "startDownload") {
+
+        console.log("Download starting...")
+
         const videos = Array.from(document.getElementsByTagName("video"));
         const urls = videos.map(v => v.getAttribute("src")).filter(Boolean);
 
+        window.alert(`Start Downloading ${videos.length} videos from ${window.location.hostname}.`)
+
         urls.forEach(async (url, index) => {
             try {
+
+                const videoName = `video${index + 1}.mp4`
+                console.log(`Start analysing ${videoName}...`)
+
                 const response = await fetch(url);
                 const blob = await response.blob();
 
+                console.log(`Finished analysing ${videoName}, start downloading...`)
+
                 const blobUrl = URL.createObjectURL(blob);
                 const a = document.createElement("a");
+
                 a.href = blobUrl;
-                a.download = `video${index + 1}.mp4`;
+                a.download = videoName;
                 document.body.appendChild(a);
                 a.click();
                 document.body.removeChild(a);
--- popup.js
+++ popup.js
@@ -1,8 +1,7 @@
-document.getElementById("start-download").addEventListener("click", () => {
-  window.alert("Clicked")
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    chrome.tabs.sendMessage(tabs[0].id, { action: "startDownload" }, (response) => {
-      console.log("Finished")
-    });
-  });
+document.getElementById("download-button").addEventListener("click", () => {
+	chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+		chrome.tabs.sendMessage(tabs[0].id, { action: "startDownload" }, (response) => {
+			console.log("Finished")
+		});
+	});
 });
