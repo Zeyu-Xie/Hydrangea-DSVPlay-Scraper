5d25e522f110a352df4b432c51f0c9e5076a3cbb
Zeyu Xie <xie.zeyu20@gmail.com>
2025-04-01 13:16:06 +0200

    .

---
+++ compress.sh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+
+zip "./DSVPlayer Scraper" \
+    background.js \
+    content.js \
+    icon16.png \
+    icon48.png \
+    icon128.png \
+    LICENSE \
+    manifest.json \
+    popup.html \
+    popup.js \
+    privacy_policy.md \
+    screenshot1280.png
